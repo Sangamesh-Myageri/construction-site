2e8d8137151d7c61c4e5a5de35955b5fe8b0dd54
Sangamesh Myageri <sangumageri@gmail.com>
2023-11-27 23:26:56 +0530

    Initial commit of contrustion site

---
+++ web/themes/custom/devconnect/js/custom.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     var navbar = document.querySelector(".site-header__inner");
+//     var footer = document.querySelector("#block-footer-2");
+//     var footerOffset = footer.offsetTop;
+    
+//     function handleScroll() {
+//       var scrollPosition = window.pageYOffset;
+      
+//       if (scrollPosition > footerOffset - window.innerHeight) {
+//         navbar.style.position = "static";
+//       } else {
+//         navbar.style.position = "fixed";
+//       }
+//     }
+  
+//     window.addEventListener("scroll", handleScroll);
+//   });
+  
